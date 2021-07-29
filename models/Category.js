2c014ead4642007e95ce8beca2133fc8904c7c44
Taegan Loyzelle <tloyzelle@gmail.com>
2021-07-29 18:41:15 -0400

    defining the columns

--- models/Category.js
+++ models/Category.js
@@ -6,7 +6,17 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    // columns defined
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
   },
   {
     sequelize,
